d2299c1c69b32f3c31b6a847d69347148a1a810d
rafaeltio <meneses.tio@gmail.com>
2015-07-15 16:58:41 -0300

    Task 2 2015.2

---
+++ Entidade/LixoDenuncia.class.php
@@ -0,0 +1,27 @@
+<?php
+namespace Entidade;
+
+use \Persistencia\Conexao;
+
+class LixoDenuncia
+{
+	//Uso posterior
+	private $nomeDoDenunciante;
+	private $emailDoDenunciante;
+	private $logradouroDoLixo;
+	private $NumeroDoLocalDoLixo;
+	private $BairroDoLixo;
+	private $TipoDoLixo;
+	private $Latitude;
+	private $Logitude;
+	private $DataDaDenuncia;
+	
+	public static function getAllData()
+	{
+		$sql = "SELECT `ID`, `Nome`, `Email`, `Logradouro`, `Numero`, `Bairro`, `TipoLixo`, `Latitude`, `Longitude`, `Data` FROM `Localizacao` WHERE 1";
+		$query = Conexao::getInstance()->query($sql);
+ 		
+		return $query->fetchAll(PDO::FETCH_ASSOC);
+	}
+}
+?>
